1a30d8d1a6331c790faf8ec8b2616cc98125a651
Roei Bracha <roei635@gmail.com>
2019-10-01 00:32:15 +0300

    adding pull to refresh

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,42 +1,57 @@
-import React , {useEffect, useState } from 'react';
-import { View, FlatList, Button, Platform, ActivityIndicator, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  Platform,
+  ActivityIndicator,
+  StyleSheet
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import * as productsActions from '../../store/actions/products';
 import Colors from '../../constants/Colors';
-import { fetchProducts } from '../../store/actions/products';
 
 const ProductsOverviewScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [error, setError] = useState();
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(undefined)
-  const loadData = async () => {
-    setError(null)
-    setIsLoading(true)
-    dispatch(fetchProducts()).then(() => {
-      setIsLoading(false)
-    }).catch((err) => {
-      setError(err)
-      setIsLoading(false)
-    })
-  }
 
-  useEffect(() => {
-    const willFocusSub  = props.navigation.addListener('willFocus', () => {
-      loadData()
-    })
-    return () => {
-      willFocusSub.remove()
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsRefreshing(true);
+    try {
+      await dispatch(productsActions.fetchProducts());
+    } catch (err) {
+      setError(err.message);
     }
-  },[loadData])
+    setIsRefreshing(false);
+  }, [dispatch, setIsLoading, setError]);
 
   useEffect(() => {
-    loadData()
-  },[dispatch])
+    const willFocusSub = props.navigation.addListener(
+      'willFocus',
+      loadProducts
+    );
+
+    return () => {
+      willFocusSub.remove();
+    };
+  }, [loadProducts]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    loadProducts().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, loadProducts]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
@@ -45,33 +60,39 @@ const ProductsOverviewScreen = props => {
     });
   };
 
-  if (isLoading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator size={'large'} color={Colors.primary} />
-      </View>
-    )
-  }
-
   if (error) {
     return (
-      <View style={styles.center}>
-        <Text> error happened </Text>
-        <Button title={'Try Again'} onPress={()=>loadData()}/>
+      <View style={styles.centered}>
+        <Text>An error occurred!</Text>
+        <Button
+          title="Try again"
+          onPress={loadProducts}
+          color={Colors.primary}
+        />
       </View>
-    )
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
   }
 
   if (!isLoading && products.length === 0) {
     return (
-      <View style={styles.center}>
-        <Text> no products found </Text>
+      <View style={styles.centered}>
+        <Text>No products found. Maybe start adding some!</Text>
       </View>
-    )
+    );
   }
 
   return (
     <FlatList
+      onRefresh={loadProducts}
+      refreshing={isRefreshing}
       data={products}
       keyExtractor={item => item.id}
       renderItem={itemData => (
@@ -132,11 +153,7 @@ ProductsOverviewScreen.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+  centered: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+});
 
 export default ProductsOverviewScreen;
