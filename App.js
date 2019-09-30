e60e7aff9308856fabf6a3191c60f41165773310
Roei Bracha <roei635@gmail.com>
2019-09-30 23:03:25 +0300

    fetch data from server with redux middleware

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
+import reduxThunk from 'redux-thunk'; 
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(reduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FlatList, Button, Platform } from 'react-native';
+import React , {useEffect, useState } from 'react';
+import { View, FlatList, Button, Platform, ActivityIndicator, StyleSheet, Text } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -7,10 +7,25 @@ import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import Colors from '../../constants/Colors';
+import { fetchProducts } from '../../store/actions/products';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(undefined)
+  const loadData = async () => {
+    setIsLoading(true)
+    dispatch(fetchProducts()).then(() => {
+      setIsLoading(false)
+    }).catch((err) => {
+      setError(err)
+      setIsLoading(false)
+    })
+  }
+  useEffect(() => {
+    loadData()
+  },[dispatch])
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
@@ -18,6 +33,31 @@ const ProductsOverviewScreen = props => {
       productTitle: title
     });
   };
+
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size={'large'} color={Colors.primary} />
+      </View>
+    )
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text> error happened </Text>
+        <Button title={'Try Again'} onPress={()=>loadData()}/>
+      </View>
+    )
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.center}>
+        <Text> no products found </Text>
+      </View>
+    )
+  }
 
   return (
     <FlatList
@@ -79,5 +119,13 @@ ProductsOverviewScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default ProductsOverviewScreen;
