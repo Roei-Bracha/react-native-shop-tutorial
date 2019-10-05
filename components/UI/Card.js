fc2fad690b1c372eb29b1c8981901932ef2828eb
Roei Bracha <roei635@gmail.com>
2019-10-05 14:08:59 +0300

    add some auth

--- components/UI/Card.js
+++ components/UI/Card.js
@@ -4,7 +4,6 @@ import { View, StyleSheet } from 'react-native';
 const Card = props => {
   return <View style={{...styles.card, ...props.style}}>{props.children}</View>;
 };
-
 const styles = StyleSheet.create({
   card: {
     shadowColor: 'black',
@@ -13,7 +12,7 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   }
 });
 
